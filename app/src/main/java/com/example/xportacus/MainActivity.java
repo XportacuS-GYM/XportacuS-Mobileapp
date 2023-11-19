58cf3584d4805e9346e91469013f584fae1451d3
Rodolfo <18170375@itculiacan.edu.mx>
2023-11-18 21:55:11 -0700

    Creación de pantalla QR

--- app/src/main/java/com/example/xportacus/MainActivity.java
+++ app/src/main/java/com/example/xportacus/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void go_to_register(View view)
     {
-        Intent register=new Intent(MainActivity.this,RegisterActivity.class);
+        Intent register=new Intent(MainActivity.this,qrActivity.class);
         startActivity(register);
     }
 
