4797d753696797af2846ac3baaff68f50b1c8129
Eduardo López <18170383@itculiacan.edu.mx>
2023-11-23 03:09:57 -0700

    inicio con barra

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,8 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.6.4")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
     implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
+
+    implementation ("com.google.android.material:material:1.0.0");
+
+
 }
